8b51582b0c171ba482ad87d1616d49ac213dcafe
Rob Probin <rob.probin@gmail.com>
2016-08-20 07:35:03 +0100

    Added copyright and license to all header files.

--- Code/Screen_Splat_Control/screen_splats.h
+++ Code/Screen_Splat_Control/screen_splats.h
@@ -1,3 +1,19 @@
+// Zex - 2D Space Trading Game with 3D graphics
+// Copyright (C) 1998-2005  Stuart Ball and Robert Probin
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //screen_splats.h
 void splat_320by240(char * src, int src_width, char * dest, int dest_width);
 void splat_640by480(char * src, int src_width, char * dest, int dest_width);
